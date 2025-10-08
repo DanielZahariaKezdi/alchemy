34659ff2b4676345baffec921c243c3de44b6323
Zaharia Kezdi Daniel <zahariakezdidaniel@gmail.com>
2025-10-08 17:24:04 +0300

    simplified models

--- models.py
+++ models.py
@@ -1,44 +1,34 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, Integer, String
 from database import Base
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-
-    # relations
-    loans: Mapped[list["Loan"]] = relationship(back_populates="user")
+    name: Mapped[str] = mapped_column(String(32), nullable=False)
+    password: Mapped[str] = mapped_column(String(32), nullable=False)
 
 class Author(Base):
-    __tablename__ = "authors"
+    __tablename__ = 'authors'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    author_name: Mapped[str] = mapped_column(nullable=False)
+    author_name: Mapped[str] = mapped_column(String(32), nullable=False)
 
     # relations
-    books: Mapped[list["Book"]] = relationship(back_populates="author")
+    books: Mapped[list['Book']] = relationship('Book', back_populates = 'author')
 
 class Book(Base):
-    __tablename__ = "books"
+    __tablename__ = 'books'
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    book_title: Mapped[str] = mapped_column(nullable=False)
+    book_title: Mapped[str] = mapped_column(String(32), nullable=False)
     author_id: Mapped[int] = mapped_column(ForeignKey("authors.id"), nullable=False)
 
     # relations
     author: Mapped["Author"] = relationship(back_populates="books")
-    loans: Mapped[list["Loan"]] = relationship(back_populates="book")
 
 class Loan(Base):
     __tablename__ = "loans"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
-    book_id: Mapped[int] = mapped_column(ForeignKey("books.id"), nullable=False)
-
-    # relations
-    user: Mapped["User"] = relationship(back_populates="loans")
-    book: Mapped["Book"] = relationship(back_populates="loans")
+    pass
--- routers/users.py
+++ routers/users.py
@@ -7,30 +7,31 @@ from models import User
 router = APIRouter(prefix = '/users', tags = ['users'])
 
 def get_db():
-    db = sessionlocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    with sessionlocal() as db:
+        users = db.query(User).all()
+        return users
 
-@router.get('/', response_model=list[UserResponse])
+@router.get('/', response_model = list[UserResponse])
 
 def get_message():
-    return {'message': 'this is the user get page'}
+    return {'message': 'this is the user get'}
 
 def get_users(db: Session = Depends(get_db)):
+    
     users = db.query(User).all()
     return users
 
-@router.post('/add', response_model=UserResponse)
+@router.post('/add', response_model = UserResponse)
 
 def post_message():
     return {'message': 'this is the user post page'}
 
 def post_users(user: UserCreate, db: Session = Depends(get_db)):
 
-    new_user = User(name=user.name, password=user.password)
+    new_user = User(name = user.name, password = user.password)
+
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
+
     return new_user
